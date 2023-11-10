c23a63d181bdea8a347f9084fb36f9b4ecf1b561
Jacob Hands <jhands@cloudflare.com>
2023-11-10 11:02:03 -0600

    Copy files

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
-import PQueue from "p-queue";
+import PQueue from 'p-queue'
+import fs from 'fs/promises'
 
 async function main() {
-  const queue = new PQueue({ concurrency: 500 });
+	console.log('starting...')
+	const queue = new PQueue({ concurrency: 30 })
+	const copyFile = async (id: number) => {
+		const data = await fs.readFile(`./assets/${id}.txt`)
+		await fs.writeFile(`./public/${id}.txt`, data)
+	}
+	for (let i = 1; i <= 1000; i++) {
+		queue.add(() => copyFile(i))
+	}
+	await queue.onIdle()
 }
 
 main()
-  .then(() => console.log("done"))
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+	.then(() => console.log('done'))
+	.catch((e) => {
+		console.error(e)
+		process.exit(1)
+	})
